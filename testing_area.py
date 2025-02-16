70c07b078edc4900cea334fe87a24f8809c760c5
StressSpren <jake-howard15@yahoo.com>
2025-02-16 19:42:23 +0000

    adujustments

--- testing_area.py
+++ testing_area.py
@@ -1,27 +1,63 @@
 import requests
 
 
-url = f"http://127.0.0.1:8000/api/books"
-response = requests.get(url)
-response.raise_for_status()
-data = response.json()
-books = data.get('results', [])
-print(books)
-keyword = "King"
-keyword = keyword.lower()
+# url = f"http://127.0.0.1:8000/api/books"
+# response = requests.get(url)
+# response.raise_for_status()
+# data = response.json()
+# books = data.get('results', [])
+# print(books)
+# keyword = "King"
+# keyword = keyword.lower()
 
-dict_book = {"matches": []}
+# dict_book = {"matches": []}
 
-for book in books:
+# for book in books:
     
-    if keyword in book['title'].lower():
-        dict_book["matches"].append(book)
+#     if keyword in book['title'].lower():
+#         dict_book["matches"].append(book)
 
-print(dict_book["matches"])
+# print(dict_book["matches"])
 
-""" 
-    so far the aim is to be able to take a user input and print out results based on the input
 
-    TODO:
-        - Create a functional searchbar
-"""
+
+# """
+#     Connecting to an API and extracting results
+# """
+# url = 'http://127.0.0.1:8000/api/books'
+# response = requests.get(url) # makes a request to the api
+# response.raise_for_status() # error checks to make sure its okay
+# data = response.json() # converts the repsonse into a json format
+
+
+# books = data.get('results', []) # assigns books to the 'results' directory
+
+
+
+
+# '''
+#     Using Patch to change the values within the database
+# '''
+# url = 'http://127.0.0.1:8000/api/books/1'
+
+# response = requests.get(url)
+# response.raise_for_status()
+
+# data = response.json()
+
+# todo={"stock": 2}
+
+# data = requests.patch(url, json=todo)
+
+
+
+
+
+# """ 
+#
+
+#     TODO:
+#         - Create a functional searchbar (Done)
+#         - Make a Basket
+
+# """
