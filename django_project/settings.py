fa9614ba248797ab233e31895e58a340c531863d
StressSpren <165693186+StressSpren@users.noreply.github.com>
2025-02-17 19:06:43 +0000

    Cart Implementation

--- django_project/settings.py
+++ django_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "apps.accounts",
     "apps.api",
     "apps.inventory",
+    "apps.cart",
 
     
 ]
--- django_project/urls.py
+++ django_project/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("api/", include('apps.api.urls')),
     path('inventory/', include('apps.inventory.urls')),
+    path('cart/', include('apps.cart.urls')),
 
                    
 ]
