4c8ce58d003c59e8d956cb4af88386b0e36b9fa9
StressSpren <165693186+StressSpren@users.noreply.github.com>
2025-02-18 23:12:52 +0000

    basket functionality improved
    each item in the basket can be removed individually as well as the entire basket itself

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -10,19 +10,23 @@ from django.contrib.auth.decorators import login_required
 def cart(request):
 
     cart_all = Cart.objects.all()
-
     cart_item = Cart.objects.filter(user_id=request.user)
 
     if request.method == "POST":
         form = Cart_Deletion_Form(request.POST)
+
         if "delete_cart" in request.POST:
             cart_item.delete()
             return redirect('cart')
+        
+        if "delete_item" in request.POST:
+            cart_id = request.POST.get("cart_id")
+            if cart_id:
+                item = Cart.objects.get(id=cart_id)
+                item.delete()
+                return redirect('cart')
 
     else:
         form = Cart_Deletion_Form()
 
-
-            
-    
     return render(request, "cart.html", {'cart_item': cart_item, 'cart': cart_all, 'form': form})
