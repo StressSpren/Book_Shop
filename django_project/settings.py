85757f581716d5a989f74d98c7426fca2f459816
StressSpren <165693186+StressSpren@users.noreply.github.com>
2025-02-22 20:45:14 +0000

    updates

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,11 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
+# Add this
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Include the root static directory
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Allows access to media file within the root directory
 MEDIA_URL = '/media/'
