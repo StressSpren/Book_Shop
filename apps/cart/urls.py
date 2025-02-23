e3bf01b40685779e3779ed29f9a13c09f6e0301f
StressSpren <165693186+StressSpren@users.noreply.github.com>
2025-02-23 22:15:51 +0000

    checkout implementation

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import Cart_Deletion_Form
 from .models import Cart
+from apps.api.models import Books
 
 @login_required
 def cart(request):
@@ -29,8 +30,28 @@ def cart(request):
                 item = get_object_or_404(Cart, id=cart_id)
                 item.delete()
                 return redirect('cart')
+        if "checkout" in request.POST:
+
+            # Update the stock of each book in the cart
+            books = Books.objects.all()
+            for item in cart_items:
+                book = get_object_or_404(books, id=item.book.id)
+                if book.stock <= 1:
+                    return redirect('cart')
+                else:
+                    book.stock -= 1
+                    book.save()
+
+            # Handle deletion of all cart items
+            items = Cart.objects.filter(user_id=request.user)
+            for item in items:
+                item.delete()
+            return redirect('checkout')
     else:
         form = Cart_Deletion_Form()
 
     # Render the cart template with the cart items, form, and total price
     return render(request, "cart.html", {'cart': cart_items, 'form': form, 'total_price': total_price})
+
+def checkout(request):
+    return render(request, "checkout.html")
