f6f7619284dd0c8940f83e66a33df385d5f1c67f
StressSpren <165693186+StressSpren@users.noreply.github.com>
2025-02-27 23:58:34 +0000

    updates

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -5,6 +5,7 @@
 from django.urls import reverse_lazy 
 from django.views.generic import CreateView
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 from .forms import CustomUserCreationForm
@@ -14,6 +15,7 @@ class SignUpView(CreateView):
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
+@login_required
 def profile_page_view(request):
     return render(request, 'profile_page.html')
 
