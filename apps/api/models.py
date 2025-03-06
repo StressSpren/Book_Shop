6432a7520a598c36ef1aac5d9661c6382bf14040
StressSpren <165693186+StressSpren@users.noreply.github.com>
2025-03-06 19:40:05 +0000

    Changes to Books model
    Sales are now automatically added when a stock reaches above 60

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from decimal import Decimal
 
 # Create your models here.
 
@@ -31,12 +32,31 @@ class Books(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="books")
     description = models.TextField()
+    original_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
     cover_image = models.ImageField(upload_to='book_pics/', blank=True, null=True)
     published_date = models.DateField()
     isbn = models.CharField(max_length=13, unique=True)
     for_sale = models.BooleanField(default=False, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+
+        # Apply discount when for_sale is first set to True
+        if self.for_sale and self.price == self.original_price:
+            self.price = self.original_price * Decimal('0.8')
+        
+        # Add item to sale when above 60 stock
+        if self.stock > 60 and not self.for_sale:
+            self.for_sale = True
+            
+
+        # Remove discount when stock is less than 10 and change for_sale to False
+        if self.for_sale and self.stock < 10 and self.price != self.original_price:
+            self.for_sale = False
+            self.price = self.original_price
+        
+        super().save(*args, **kwargs)
 
     def clean(self):
         if self.price < 0:
